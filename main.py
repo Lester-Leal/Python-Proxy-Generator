87bab97f9ac8340d716b1e22eefdbfbf353b7596
Lester <93441579+Lester-Leal@users.noreply.github.com>
2022-12-05 17:54:49 +0800

    feat: Proxy Check Slow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import urllib.request , socket
 from rich import print, pretty
+from rich.table import Table
 from rich.console import Console
 from time import sleep
 import requests
@@ -7,24 +9,52 @@ import json
 pretty.install()
 console = Console()
 
+socket.setdefaulttimeout(180)
+
+def is_bad_proxy(pip):
+    try:        
+        proxy_handler = urllib.request.ProxyHandler({'http': pip})        
+        opener = urllib.request.build_opener(proxy_handler)
+        opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+        urllib.request.install_opener(opener)        
+        sock=urllib.request.urlopen('http://www.google.com')  # change the url address here
+    except urllib.error.HTTPError as e:        
+        print('Error code: ', e.code)
+        return e.code
+    except Exception as detail:
+
+        print( "ERROR:", detail)
+        return 1
+    return 0
+
 def get_ipandport(URL):
     response = requests.get(URL)
     proxies = json.loads(response.text)
     ip = []
     port = []
     country = []
+    proxyList = []
     for proxy in proxies['data']:
         ip.append(proxy['ip'])
         port.append(proxy['port'])
         country.append(proxy['country'])
-    
+        proxyList.append(proxy['ip'] + ':' + str(proxy['port']))
     for i in range(len(ip)):
-        print(ip[i] + ':' + port[i])
+        print('Getting proxy: ' + ip[i] + ':' + str(port[i]))
         with open('proxies.txt', 'a') as f:
             f.write(ip[i] + ':' + port[i] + " # " + country[i] + '\n')
         with console.status("\n[bold green]Getting Proxies...") as status:
             sleep(0.05)
     console.print("\n[bold green]Done! Check the file 'proxies.txt'")
+    print(' ')
+    for i in range(len(proxyList)):
+        if is_bad_proxy(proxyList[i]):
+            print("Bad Proxy", proxyList[i])
+        else:
+            print("Good Proxy", proxyList[i])
+            # save good proxy to file
+            with open('good_proxies.txt', 'a') as f:
+                f.write(proxyList[i] + " # " + country[i] + '\n')
     return ip, port
     
 def PROTOCOL_HTTP():
